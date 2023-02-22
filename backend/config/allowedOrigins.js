e58a9f2b5c98c62815e20c15aee9b78ef927994e
RhythmAgg <aggarwalrhythm2211@gmail.com>
2023-02-22 11:51:46 +0530

    final updates

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://greddiiit.onrender.com',
+    'http://localhost:8080'
+]
+
+module.exports = allowedOrigins
--- react_part_1_backend/config/allowedOrigins.js
+++
@@ -1,6 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://greddiiit.onrender.com'
-]
-
-module.exports = allowedOrigins
